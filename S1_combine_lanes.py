143fa30db01ba67d1c08ff5ec056d16f6b118a3e
Naici-Guo <147424703+Naici-Guo@users.noreply.github.com>
2025-10-01 12:08:35 +0100

    Change to the latest version

--- S1_combine_lanes.py
+++ S1_combine_lanes.py
@@ -9,12 +9,13 @@ from ruffus import *
 from cgatcore import pipeline as P
 import glob
 import sys
+import os
+
 ALL_FILES = glob.glob("*.fq.gz")
 REGEX_R1 = r"(R[1-4]_(?:dTAG|WT)_(?:IN|PD)_[A-Z0-9-]+)_.*_L\d+_1\.fq\.gz"
 REGEX_R2 = r"(R[1-4]_(?:dTAG|WT)_(?:IN|PD)_[A-Z0-9-]+)_.*_L\d+_2\.fq\.gz"
 
 # combine R1 files from multiple lanes
-
 @collate(ALL_FILES,
          regex(REGEX_R1),
          r"merged_data/\1_combined_1.fq.gz")
@@ -28,13 +29,13 @@ def combine_R1(infiles, outfile):
 @collate(ALL_FILES,
          regex(REGEX_R2),
          r"merged_data/\1_combined_2.fq.gz")
-def combine_R1(infiles, outfile):
+def combine_R2(infiles, outfile):
     outdir = os.path.dirname(outfile)
     infiles_str = " ".join(infiles)
     statement = f'''mkdir -p {outdir} && zcat {infiles_str} | gzip > {outfile}'''
     P.run(statement, job_memory="8G", job_threads=1)
 
-@follows([combine_R1, combine_R2])
+@follows(combine_R1, combine_R2)
 def full():
     pass
 def main(argv=None):
---
+++ S2_qc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=S2_qc
+#SBATCH --time=4:00:00
+#SBATCH --mem=8G
+#SBATCH --cpus-per-task=2
+
+module load FastQC/0.12.1-Java-11
+mkdir -p qc_reports
+
+fastqc data_merged/*.fq.gz --threads 2 --outdir qc_reports
+
+echo "Report available at: qc_reports/multiqc_report.html"
+#multiqc qc_reports -o qc_reports
