5b13c67512ee13915f1bdff50bf42549ef0eedba
Naici-Guo <147424703+Naici-Guo@users.noreply.github.com>
2025-11-04 10:40:39 +0000

    changed mode for compute matrix for genes

--- S8_ComputeMatrix_genes.py
+++ S8_ComputeMatrix_genes.py
@@ -1,6 +1,6 @@
 """
 :Author: Naici_G
-:Date: 27 Oct 2025
+:Date: 3 Nov 2025
 :Description: Compute group matrix from log2-ratio bigwig files for specific genes (i.e.,FOS) using computeMatrix from deepTools.
 
 :Starting files: all *.log2ratio.bw files in the working directory.
@@ -12,6 +12,8 @@ import glob
 import sys
 import os
 
+# There are three specific genes we are interested in
+# FOS, SRF & CCN2
 GENE = "FOS"
 INDIVIDUAL_BW_FILES = glob.glob("*.log2ratio.bw")
 @collate(INDIVIDUAL_BW_FILES,
@@ -19,14 +21,17 @@ INDIVIDUAL_BW_FILES = glob.glob("*.log2ratio.bw")
          [f"{GENE}.matrix.gz",
           f"{GENE}.tab",
           f"{GENE}.bed"])
+
+# gene is not the same with TSS - shouldn't be using reference-point center?
 def compute_group_matrix_gene_FOS(infiles, outfiles):
     threads = 4
     input_strings = " ".join(infiles)
     outfile_matrix, outfile_name_matrix, outfile_sorted_regions = outfiles
-    statement = f"""computeMatrix reference-point \
-                --referencePoint center \
-                -b 5000 -a 5000 \
+    statement = f"""computeMatrix scale-regions \
                 -R FOS_gene.bed \
+                --regionBodyLength 1000 \
+                --beforeRegionStartLength 1000 \
+                --afterRegionStartLength 1000 \
                 -S {input_strings} \
                 -o {outfile_matrix} \
                 -p {threads} \
